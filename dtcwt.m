6f6f82f444e8c772b400b1b0e896c78dd07df985
Rizky Haqiqi <rzkhqq@gmail.com>
2016-12-15 07:03:05 +0700

    DTCWT, with the processed image ata every data folder. put at the folder dtcwt inside, adding numbering _x_y.png x is the orientation (1,2,3) y is the tree's number (1,2)

---
+++ dtcwt.m
@@ -0,0 +1,26 @@
+dirc ='Brodatz/Testing/scaled/';
+imagefiles=dir(strcat(dirc,'*.jpg'));      
+nfiles = length(imagefiles);
+
+for idx=1:nfiles
+    name = strcat(dirc,imagefiles(idx).name);
+    curr = imread(name);
+    inpgray = double(rgb2gray(curr));
+    %imagesc(inpgray);
+    
+    wt = dddtree2('cplxdt',inpgray,1,'FSfarras','qshift10');
+    wav = wt.cfs{1};
+    for i=1:6
+        x = i;
+        y=1;
+        if i > 3
+            x = x-3;
+            y=2;
+        end
+       
+        A = wav(:,:,x,y,1);
+        imwrite(A,strcat(dirc,'dtcwt/',imagefiles(idx).name,'_',num2str(y),'_',num2str(x),'.png'));
+    end
+    
+end
+    
